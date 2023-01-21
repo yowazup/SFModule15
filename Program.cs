9ae5cd8f5942ff6170cf4aa4a54d6e3b76cec088
Nikita <nikit.zakharov@gmail.com>
2023-01-21 22:22:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Net.WebSockets;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+            Console.ReadKey();
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var result = new List<string>();
+
+            foreach (var c in classes)
+            {
+                foreach (var student in c.Students)
+                    result.Add(student);
+            }
+            return result.ToArray();
+        }
+    }
+
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+
+}
